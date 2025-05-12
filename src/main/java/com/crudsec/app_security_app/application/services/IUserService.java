3923fd39a0ae612b3e82cdcab7e8d5e465de2b82
Stivenco12 <153353269+Stivenco12@users.noreply.github.com>
2025-05-12 16:50:35 -0500

    feat:✨implement user and role management with security configuration

---
+++ src/main/java/com/crudsec/app_security_app/application/services/IUserService.java
@@ -0,0 +1,9 @@
+package com.crudsec.app_security_app.application.services;
+
+import java.util.List;
+import com.crudsec.app_security_app.domain.entity.User;
+
+public interface IUserService {
+    List<User> findAll();
+    User save(User user);
+}
